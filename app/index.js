9d76dee4af7f443f88a02c9c39d0b1a3b1b123c5
stonyDeveloper <stoneachi@gmail.com>
2023-06-19 11:36:00 +0100

    react_native_jobs

--- app/index.js
+++ app/index.js
@@ -1,13 +1,47 @@
-import {View, Text} from 'react-native'
-
+import { View, ScrollView, SafeAreaView, Text } from "react-native";
+import { useState } from "react";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  Welcome,
+  ScreenHeaderBtn,
+} from "../components";
 
 const Home = () => {
-    return (
-        <View>
-            <Text>Home</Text>
+  const router = useRouter();
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+
+      <ScrollView>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
         </View>
-    )
-}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-
-export default Home
+export default Home;
